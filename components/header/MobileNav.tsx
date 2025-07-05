d14da33fde21882d36f202d15645633bf9b03331
Adetunji Aramide <adetunjiaramide@gmail.com>
2025-07-05 12:41:47 +0100

    updated mobile nav

--- components/header/MobileNav.tsx
+++ components/header/MobileNav.tsx
@@ -4,33 +4,45 @@ import React from "react";
 import Image from "next/image"
 import Link from "next/link"
 import { FiMenu } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
 
 export default function MobileNav() {
 
-    const [toggleMenu, setToggleMenu] = React.useState(false);
+ const [toggleMenu, setToggleMenu] = React.useState(false);
   return (
     <div className="flex justify-between items-center">
             <h1> <Image src="/Logo.svg" alt="logo" width={204} height={50}/></h1>
-            
             <div>
-                <FiMenu />
+                <button onClick={() => setToggleMenu(true)}>
+                        <FiMenu />
+                </button>
             </div>
+           {toggleMenu && (
+            <div
+                className={`fixed top-0 left-0 h-full w-[80%] bg-white z-40 px-8 py-6 flex flex-col transform transition-transform duration-300 ease-in-out ${
+                    toggleMenu ? 'translate-x-0' : '-translate-x-full' 
+                }`}
+                >
+                <div className="flex justify-end mb-6">
+                    <button onClick={() => setToggleMenu(false)}>
+                        <MdClose size={26} />
+                    </button>
+                </div>
 
-           { toggleMenu && (
-                <div>
-                    <div className="flex space-x-6 justify-between">
-                        <Link className="text-[#36B864] font-semibold" href="#">Home</Link>
-                        <Link className="hover:text-[#36B864]" href="#">About us</Link>
-                        <Link className="hover:text-[#36B864]" href="#">Services</Link>
-                        <Link className="hover:text-[#36B864]" href="#">Blog</Link>
-                        <Link className="hover:text-[#36B864]" href="#">Contact</Link>
-                    </div>
-                    <div>
-                        <button className="px-8 py-3 border rounded-md bg-[#36B864] text-white text-sm">
-                        <Link href="#">Get a Quote</Link></button>
-                    </div>
+                <div className="flex flex-col space-y-6 items-start">
+                <Link className="text-[#36B864] font-semibold text-lg" href="#">Home</Link>
+                <Link className="hover:text-[#36B864] text-lg" href="#">About us</Link>
+                <Link className="hover:text-[#36B864] text-lg" href="#">Services</Link>
+                <Link className="hover:text-[#36B864] text-lg" href="#">Blog</Link>
+                <Link className="hover:text-[#36B864] text-lg" href="#">Contact</Link>
+
+                <button className="mt-6 px-8 py-3 border rounded-md bg-[#36B864] text-white text-sm">
+                    <Link href="#">Get a Quote</Link>
+                </button>
+                </div>
             </div>
-           )}    
+            )}
+  
     </div>
   )
 }
